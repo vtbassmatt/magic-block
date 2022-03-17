fdd174b5144cd454b5f798eea7f18542950a7ead
Matt Cooper <vtbassmatt@gmail.com>
2022-03-17 06:08:08 -0400

    cache Scryfall responses

--- src/blocks/decklist-block/cardData.ts
+++ src/blocks/decklist-block/cardData.ts
@@ -9,17 +9,25 @@ class CardDB {
   private cards: CardMap = {};
 
   async getCard(cardname: string): Promise<Card> {
+    if (cardname in this.cards) {
+      const maybeCardData = this.cards[cardname];
+      if (maybeCardData) return Promise.resolve(maybeCardData);
+      return Promise.reject("card was not found");
+    }
+
     let self = this;
-    let result = getCardNamed(cardname)
+    return getCardNamed(cardname)
       .then(function (card) {
         self.cards[cardname] = card;
         return card;
       })
       .catch(function (err) {
         self.cards[cardname] = false;
+        if (err?.originalError?.status == 404) {
+          return Promise.reject("card not found on Scryfall");
+        }
         throw err;
       });
-    return result;
   }
 }
 
